7c044b6bcf4ead488e088c18a624a2ece07e589d
Diego-Garza2007 <diegoe.garza2007@gmail.com>
2024-11-03 09:53:23 -0600

    beta 1.1.4

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const Stock = require('./models/Stock');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000 || 8080;
 
 app.use(cors());
 app.use(express.json());
